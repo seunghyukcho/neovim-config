0d61516c18a7260905fb8b9c92d31b44c48c827b
seunghyukcho <shhj1998@postech.ac.kr>
2025-08-21 23:08:38 +0900

    Modify copilot chat setting

--- lua/plugins/copilot_chat.lua
+++ lua/plugins/copilot_chat.lua
@@ -10,10 +10,10 @@ return {
       debug = false, -- 디버그 모드 비활성화
       model = "claude-sonnet-4",
       window = {
-        layout = "float", -- 플로팅 윈도우로 열기
+        -- layout = "float", -- 플로팅 윈도우로 열기
         width = 0.4,
-        height = 0.5,
-        border = "rounded",
+        -- height = 0.5,
+        -- border = "rounded",
       },
       mappings = {
         close = { "<Esc>", "<C-c>" },
@@ -21,37 +21,38 @@ return {
         reset = "<C-r>", -- 세션 초기화
       },
     },
-    config = function(_, opts)
-      require("CopilotChat").setup(opts)
-
-      -- 단축키 설정 예시
-      local map = vim.keymap.set
-      local chat = require("CopilotChat")
-      local select = require("CopilotChat.select")
-
-      map("n", "<leader>ccq", function()
-        local input = vim.fn.input("Quick Chat: ")
-        if input ~= "" then
-          require("CopilotChat").ask(input, {
-            selection = select.buffer,
-          })
-        end
-      end, { desc = "CopilotChat - Quick chat" })
-
-      map("v", "<leader>ccq", function()
-        local input = vim.fn.input("Quick Chat: ")
-        if input ~= "" then
-          require("CopilotChat").ask(input, {
-            selection = select.visual,
-          })
-        end
-      end, { desc = "CopilotChat - Quick chat" })
-
-      -- Copilot Chat 창 열기
-      map("n", "<leader>cch", function()
-        chat.toggle()
-      end, { desc = "Copilot Chat: 대화창 열기/닫기" })
-    end,
+    keys = {
+      {
+        "<leader>ccq",
+        mode = { "v" },
+        function()
+          local input = vim.fn.input("Quick Chat: ")
+          if input ~= "" then
+            require("CopilotChat").ask(input, {
+              selection = require("CopilotChat.select").visual,
+            })
+          end
+        end,
+        desc = "CopilotChat Question",
+      },
+      { "<leader>cca", "<C-y>", mode = { "n", "i" }, desc = "CopilotChat Accept nearest diff" },
+      {
+        "<leader>cch",
+        mode = "n",
+        function()
+          require("CopilotChat").toggle()
+        end,
+        desc = "Copilot Chat: 대화창 열기/닫기",
+      },
+      {
+        "<leader>ccr",
+        mode = "n",
+        function()
+          require("CopilotChat").reset()
+        end,
+        desc = "Copilot Chat: 세션 초기화",
+      },
+    },
     event = "VeryLazy",
   },
 }
